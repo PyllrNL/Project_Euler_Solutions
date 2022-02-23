e91846413be98f0530218f0589e62610c65eb33a
Folkert Kevelam <folkert@pyllr.nl>
2022-02-23 12:39:56 +0100

    Change Integer to Int64 to increase solution space

--- Ada/problem_1.adb
+++ Ada/problem_1.adb
@@ -2,10 +2,11 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Calendar; use Ada.Calendar;
 
 procedure Problem_1 is
-    Sum_Of_Multiples: Integer := 0;
 
-    function Solve_1( Max : Integer ) return Integer is
-        Sum : Integer := 0;
+    type Int64 is range -2**63 .. 2**63 - 1;
+
+    function Solve_1( Max : Int64 ) return Int64 is
+        Sum : Int64 := 0;
     begin
         for I in 3 .. Max - 1 loop
             if I mod 3 = 0 or else I mod 5 = 0 then
@@ -16,13 +17,13 @@ procedure Problem_1 is
         return Sum;
     end Solve_1;
 
-    function Solve_2( Max : Integer ) return Integer is
-        Sum : Integer := 0;
+    function Solve_2( Max : Int64 ) return Int64 is
+        Sum : Int64 := 0;
 
-        function Arithmetic_Sequence( N, Top : Integer ) return Integer is
-            First_Term : Integer := N;
-            Last_Term : Integer := Top - ( Top mod N );
-            N_Terms : Integer := Last_Term / N;
+        function Arithmetic_Sequence( N, Top : Int64 ) return Int64 is
+            First_Term : Int64 := N;
+            Last_Term : Int64 := Top - ( Top mod N );
+            N_Terms : Int64 := Last_Term / N;
         begin
             return N_Terms * ( First_Term + Last_Term) / 2;
         end Arithmetic_Sequence;
@@ -34,8 +35,8 @@ procedure Problem_1 is
         return Sum;
     end Solve_2;
 
-    Solution : Integer := 233168;
-    Test : Integer;
+    Solution : Int64 := 233168;
+    Test : Int64;
 
     Start_Time, End_Time : Time;
     Dur : Duration;
@@ -45,14 +46,10 @@ begin
     Put_Line("Running first Solution");
     Start_Time := Clock;
     for I in 1 .. 1_000 loop
-        Test := Solve_1(1_000);
+        Test := Solve_1(1_000_000_0);
     end loop;
     End_Time := Clock;
     Dur := (End_Time - Start_Time) / 1_000;
-
-    if Test = Solution then
-        Put_Line("Expected Result of" & Integer'Image(Test) & " Has been returned");
-    end if;
 
     Put_Line("Runtime(Avg) = " & Duration'Image(Dur) & " ms");
 
@@ -61,14 +58,10 @@ begin
     Put_Line("Running second solution");
     Start_Time := Clock;
     for I in 1 .. 1_000 loop
-        Test := Solve_1(1_000);
+        Test := Solve_1(1_000_000_0);
     end loop;
     End_Time := Clock;
     Dur := (End_Time - Start_Time) / 1_000;
-
-    if Test = Solution then
-        Put_Line("Expected Result of" & Integer'Image(Test) & " Has been returned");
-    end if;
 
     Put_Line("Runtime(Avg) = " & Duration'Image(Dur) & " ms");
 end Problem_1;
