d4397578acd44ee5625a2657e1063bf7f84bc3c0
Folkert Kevelam <folkert@pyllr.nl>
2022-02-25 15:06:40 +0100

    Change array into vector

--- Ada/problem_3.adb
+++ Ada/problem_3.adb
@@ -2,6 +2,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics; use Ada.Numerics;
 with Ada.Numerics.Elementary_Functions;
 use Ada.Numerics.Elementary_Functions;
+with Ada.Containers.Vectors;
 
 procedure problem_3 is
 
@@ -12,18 +13,18 @@ procedure problem_3 is
         Count : Integer;
     end record;
 
-    type Prime_List is array (Natural range <>) of Prime_Count;
+    package P is new Ada.Containers.Vectors(Index_Type => Natural,
+    Element_Type => Prime_Count);
 
-    function Prime_Factorization( Num : Int64 ) return Prime_List is
+    function Prime_Factorization( Num : Int64 ) return P.Vector is
         Upper : constant Int64 := Int64(Sqrt(Float(Num)));
         N : Int64 := Num;
         I : Int64 := 3;
         Inc : Integer := 0;
         Count : Integer := 0;
-        Prime_Upper_Bound : Int64 := Upper / (Int64(Log(Float(Upper), Float(10))));
-        Primes : Prime_List(0 .. Integer(Prime_Upper_Bound));
+        Prime : Prime_Count;
+        Primes : P.Vector;
     begin
-        Put_Line(Int64'Image(Prime_Upper_Bound));
         if N mod 2 = 0 then
             Count := 1;
             N := N / 2;
@@ -31,7 +32,8 @@ procedure problem_3 is
                 Count := Count + 1;
                 N := N / 2;
             end loop;
-            Primes(Inc) := (Prime => 2, Count => Count);
+            Prime := (Prime => 2, Count => Count);
+            Primes.append( Prime );
             Inc := Inc + 1;
         end if;
 
@@ -41,26 +43,24 @@ procedure problem_3 is
                 Count := 1;
                 N := N / I;
                 while N mod I = 0 loop
-                    Primes(Inc).Count := Primes(Inc).Count + 1;
+                    Count := Count + 1;
                     N := N / I;
                 end loop;
-                Primes(Inc) := ( Prime => Integer(I), Count => Count );
+                Prime := (Prime => Integer(I), Count => Count);
+                Primes.append( Prime );
                 Inc := Inc + 1;
             end if;
             I := I + 2;
         end loop outer;
 
-        declare
-            Primes_Ret : Prime_List(0 .. Inc - 1) := Primes(0 .. Inc - 1);
-        begin
-            return Primes_Ret;
-        end;
+        return Primes;
+
     end Prime_Factorization;
 
     function Solve_1( Comp : Int64 ) return Int64 is
-        Primes : constant Prime_List := Prime_Factorization(600851475143);
+        Primes : P.Vector := Prime_Factorization(600851475143);
     begin
-        return Int64(Primes(Primes'Last).Prime);
+        return Int64(Primes.Last_Element.Prime);
     end Solve_1;
 
 begin
