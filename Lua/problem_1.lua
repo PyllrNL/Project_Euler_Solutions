cc90c7633c5bfc7c5cd54cedc43dc30f4e855629
Folkert Kevelam <folkert@pyllr.nl>
2022-03-01 11:43:32 +0100

    Add 2nd solution to Problem 1

--- Lua/problem_1.lua
+++ Lua/problem_1.lua
@@ -1,6 +1,6 @@
-function Solve_1()
+function Solve_1(Num)
     Sum = 0
-    for i = 1, 999, 1 do
+    for i = 1, Num - 1, 1 do
         if i % 3 == 0 or i % 5 == 0 then
             Sum = Sum + i
         end
@@ -9,4 +9,21 @@ function Solve_1()
     return Sum
 end
 
-print(Solve_1())
+function Arith_Sequence( N, Top )
+    First = N
+    Last = Top - ( Top % N )
+    Terms = Last / N
+
+    return (Terms * ( First + Last ) / 2);
+end
+
+function Solve_2(Num)
+    Sum = Arith_Sequence( 3, Num - 1 )
+    Sum = Sum + Arith_Sequence( 5, Num - 1)
+    Sum = Sum - Arith_Sequence( 15, Num - 1)
+
+    return (Sum | 0)
+end
+
+print(Solve_1(1000))
+print(Solve_2(1000))
