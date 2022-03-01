87e358f36135739f5531668b97ded57e1b6b794d
Folkert Kevelam <folkert@pyllr.nl>
2022-03-01 09:32:38 +0100

    Seperate logic into Solve_1 function

--- Ada/problem_7.adb
+++ Ada/problem_7.adb
@@ -7,29 +7,37 @@ procedure problem_7 is
 
     package P is new Ada.Containers.Vectors(Index_Type => Natural, Element_Type => Int64);
 
-    Prime_Count : Int64 := 1;
-    Inc : Int64 := 3;
-    Primes : P.Vector;
-    Divisible : Boolean;
+    function Solve_1( Num : Integer ) return Int64 is
+        Primes : P.Vector;
+        Prime_Count : Int64 := 1;
+        Inc : Int64 := 3;
+        Divisible : Boolean;
+    begin
+        if Num < 2 then
+            return 0;
+        end if;
+
+        Primes.append(2);
+
+        while Prime_Count /= Int64(Num) loop
+            Divisible := false;
+            for E of Primes loop
+                if Inc mod E = 0 then
+                    Divisible := Divisible or true;
+                end if;
+            end loop;
+            if Divisible = false then
+                Primes.append(Inc);
+                Prime_Count := Prime_Count + 1;
+            end if;
+            Inc := Inc + 2;
+        end loop;
+
+        return Primes.Last_Element;
+    end Solve_1;
 
 begin
 
-    Primes.append(2);
-
-    while Prime_Count /= 10_001 loop
-        Divisible := false;
-        for E of Primes loop
-            if Inc mod E = 0 then
-                Divisible := Divisible or true;
-            end if;
-        end loop;
-        if Divisible = false then
-            Primes.append(Inc);
-            Prime_Count := Prime_Count + 1;
-        end if;
-        Inc := Inc + 2;
-    end loop;
-
-    Put_Line(Int64'Image(Primes.Last_Element));
+    Put_Line(Int64'Image(Solve_1(10_001)));
 
 end problem_7;
